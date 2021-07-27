722ca0e788b03fda9b09a6a6b4bfbedcbc9b5614
Uthra1990 <uthraravichandran29@gmail.com>
2021-07-27 00:10:42 -0400

    assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  if (expected === actual) {
+    console.log('Assertion Passed:' + actual + ' === ' + expected);
+  } else {
+    console.log('Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+/* if(assertEqual === true)
+{
+    console.log( Assertion Passed: ["Lighthouse labs"] === ["Bootcamp"])
+}
+else
+{
+    console.log(Assertion Failed: ["Lighthouse labs"] !== ["Bootcamp"])
+}  */
