96801eacded8d7a3c556c63c9c8d00d9455ddc0c
Uthra1990 <uthraravichandran29@gmail.com>
2021-07-29 17:52:33 -0400

    tail.js

---
+++ tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+    if (expected === actual) {
+      console.log('Assertion Passed:' + actual + ' === ' + expected);
+    } else {
+      console.log('Assertion Failed: ' + actual + ' !== ' + expected);
+    }
+};
+
+let tail = function(arr) {
+    let len = arr.length
+    let tempArr = []
+    if(len !=0)
+    {
+        tempArr = arr
+        tempArr.shift()
+        return tempArr
+    }
+  }
+  assertEqual(tail([5,6,7]),[6,7]);
+  assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse","Labs"]);
+  assertEqual(tail([6]), []);
+  assertEqual(tail([]), "Hello");
+  
