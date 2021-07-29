2a4902388925928e61daf9b59efe9157540c0efd
Uthra1990 <uthraravichandran29@gmail.com>
2021-07-29 15:43:18 -0400

    head.js

---
+++ head.js
@@ -0,0 +1,22 @@
+let head = function(arr) {
+
+  if(arr[0])
+    // console.log(arr[0])
+    return arr[0]
+}
+const assertEqual = function(actual, expected) {
+  if (expected === actual) {
+    console.log('Assertion Passed:' + actual + ' === ' + expected);
+  } else {
+    console.log('Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([6]), 6);
+assertEqual(head([]), "Hello");
+
+  
