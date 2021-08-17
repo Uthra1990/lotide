48f4ee8381a530a656c6ccbf1dc05c7563033385
Uthra1990 <uthraravichandran29@gmail.com>
2021-08-17 10:24:55 -0400

    tests pushed

---
+++ test/assertEqualTest.js
@@ -0,0 +1,13 @@
+// TEST CODE
+const assertEqual = require('../assertEqual');
+
+ assertEqual("Lighthouse Labs", "Bootcamp");
+ assertEqual(1,1);
+/*if(assertEqual === true)
+{
+    console.log('Assertion Passed:' ["Lighthouse labs"] === ["Bootcamp"])
+}
+else
+{
+    console.log('Assertion Failed:' ["Lighthouse labs"] !== ["Bootcamp"])
+}  */
