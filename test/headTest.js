ca861d7f9bc0423740eb592c992469cd495d7c4a
Uthra1990 <uthraravichandran29@gmail.com>
2021-08-17 10:50:22 -0400

    headTest tests

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,23 @@
 const head = require('../head');
 const assertEqual = require('../assertEqual');
+const { assert } = require("chai");
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([6]), 6);
 assertEqual(head([]), "Hello"); 
+
+describe("#head", () => {
+    it("should return 5 as the head", () => {
+        assert.deepEqual(head([5,6,7]), 5)
+    });
+    it("should  return Hello as head", () => {
+        assert.deepEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+    });
+    it("should  return 6 as head", () => {
+        assert.deepEqual(head([6]), 6)
+    });
+    it("should  return undefined as head", () => {
+        assert.deepEqual(head([]), "Hello")
+    });
+});
